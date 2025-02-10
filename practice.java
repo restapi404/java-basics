c50d37f9919b9adb66e14d822a3ae301bcf15edb
restapi404 <rithuprabhu23@gmail,com>
2025-02-10 13:40:22 +0530

    practice.java

--- practice.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class practice{
-	public static void main(String[] args){
-		Scanner input = new Scanner(System.in);
-		int num= input.nextInt();
-		System.out.println("entered:"+ num);
-		input.close();
-	}
-}
