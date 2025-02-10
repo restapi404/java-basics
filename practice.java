d860f8874d21f71f5cdf38b70b677566d7245507
restapi404 <rithuprabhu23@gmail,com>
2025-02-10 13:34:41 +0530

    1st week

---
+++ practice.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class practice{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int num= input.nextInt();
+		System.out.println("entered:"+ num);
+		input.close();
+	}
+}
